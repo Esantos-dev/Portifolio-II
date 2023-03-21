2710c2f1f6facfe3d7023edad0423b6a1566fef8
Esantos-dev <dosemanuel303@gmail.com>
2023-03-21 17:30:40 +0100

    script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
+/*Este código serve para fazer  o scroll  pelo  menu*/
 window.onscroll = () =>{
 let header = document.querySelector('.header');
-
 header.classList.toggle('sticky', window.scrollY > 100)
 }
+
+/*Este trecho cuidará do slide */
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween:50,
+    loop: true,
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
