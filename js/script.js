60ecd5fd61088e9e5f637668edb0aed0b76dacb0
Esantos-dev <dosemanuel303@gmail.com>
2023-03-23 17:21:08 +0100

    script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
+//Scroll sections active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+
 /*Este código serve para fazer  o scroll  pelo  menu*/
 window.onscroll = () =>{
+  sections.forEach(sec=>{
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height)
+    {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+      });
+
+    };
+  });
+
+
+
 let header = document.querySelector('.header');
 header.classList.toggle('sticky', window.scrollY > 100)
 }
